58a8d63b1a0f4231624d13e78914906582506c7b
Mario Mejia <mario128mex@gmail.com>
2021-02-08 11:14:58 -0600

    added logging configuration

--- src/bin/www.js
+++ src/bin/www.js
@@ -1,10 +1,11 @@
 const http = require('http');
 const app = require('../app');
 const {appConfig} = require('../config');
+const log = require('../logging');
 
 const server = http.createServer(app);
 const {port} = appConfig;
 
 server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  log.info(`Server is running on port ${port}`);
 });
