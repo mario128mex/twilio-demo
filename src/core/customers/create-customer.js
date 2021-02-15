ccdd6de6ee8808020d6e01b29ed8e0e088d847a6
Mario Mejia <mario128mex@gmail.com>
2021-02-14 22:02:17 -0600

    on Specialist and Customer creation, save phone with country code

--- src/core/customers/create-customer.js
+++ src/core/customers/create-customer.js
@@ -1,6 +1,10 @@
 const CustomerModel = require('../../models/Customer');
+const {countryCodes} = require('../../config');
 
 module.exports = params => {
-  const customer = new CustomerModel(params);
+  const {country, phone} = params;
+  const phoneWithCountryCode = countryCodes[country.toUpperCase()] + phone;
+
+  const customer = new CustomerModel({...params, phone: phoneWithCountryCode});
   return customer.save();
 };
--- src/core/specialists/create-specialist.js
+++ src/core/specialists/create-specialist.js
@@ -1,6 +1,10 @@
 const SpecialistModel = require('../../models/Specialist');
+const {countryCodes} = require('../../config');
 
 module.exports = params => {
-  const specialist = new SpecialistModel(params);
+  const {country, phone} = params;
+  const phoneWithCountryCode = countryCodes[country.toUpperCase()] + phone;
+
+  const specialist = new SpecialistModel({...params, phone: phoneWithCountryCode});
   return specialist.save();
 };
